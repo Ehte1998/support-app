ff14491201588d241d2c71199078d4d26302b0d8
Ehte1998 <ehteshamhusain1998@gmail.com>
2025-10-09 19:28:14 +0530

    Update project code

--- server.js
+++ server.js
@@ -343,11 +343,7 @@ async function createCashfreeOrder(amount, messageId, customerDetails = {}) {
     if (response.data.payment_session_id) {
       // Cashfree v2 uses payment_session_id to generate the payment link
       const isSandbox = CASHFREE_API_BASE.includes('sandbox');
-      const checkoutBaseURL = isSandbox
-        ? 'https://sandbox.cashfree.com'
-        : 'https://api.cashfree.com';
-
-      const paymentLink = `${checkoutBaseURL}/pg/view/order/${response.data.cf_order_id}`;
+      const paymentLink = `https://payments${isSandbox ? '-test' : ''}.cashfree.com/order?session_id=${response.data.payment_session_id}`;
       
       const result = {
         success: true,
